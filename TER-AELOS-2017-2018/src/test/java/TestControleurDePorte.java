fb2283cfe2ba887e23a280dbffd7aec136852866
loic44650 <loic44650@hotmail.fr>
2018-02-09 10:49:17 +0100

    ajout test

---
+++ TER-AELOS-2017-2018/src/test/java/TestControleurDePorte.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+class TestControleurDePorte
+{
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void () {
+
+	}
+
+}
